db806f250d8b60edebeff294c2a7c6d5153f5016
Kiaran <klearyire@gmail.com>
2021-09-30 15:12:34 -0400

    exercises not done, very confused

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,8 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student kiaran = new Student("Kiaran", 5, 1, 4.0);
+
         }
     }
 }
