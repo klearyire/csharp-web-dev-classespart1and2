9d465dfa18137160d83a42ab563ec9c53bf121d4
Kiaran <klearyire@gmail.com>
2021-10-03 14:32:12 -0400

    created GetGradeLevel

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -7,20 +7,67 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name { get; set; }
-        private int StudentId { get; set; }
-        private int NumberOfCredits { get; set; }
-        private double Gpa { get; set; }
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        private readonly int studentId;
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
 
-        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        public Student(string name, int sId, int numberOfCredits, double gpa)
         {
             Name = name;
-            StudentId = studentId;
+            studentId = sId;
             NumberOfCredits = numberOfCredits;
             Gpa = gpa;
         }
 
-        public Student(string name, int studentId) : this(name, studentId, 0, 0.0) { }
+        public Student(string name, int sId)
+        {
+            Name = name;
+            studentId = sId;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public Student(string name)
+        {
+            Name = name;
+            studentId = nextStudentId;
+            nextStudentId++;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public string StudentInfo()
+        {
+            return (Name + " has a GPA of: " + Gpa);
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // update fields
+        }
+
+        public string GetGradeLevel (int NumberOfCredits)
+        {
+            //Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits)
+            if (NumberOfCredits <= 29)
+                {
+                    return "Freshman";
+                }
+                else if (NumberOfCredits >= 30 && NumberOfCredits <= 59)
+                { 
+                    return "Sophmore";
+                }
+                else if (NumberOfCredits >= 60 && NumberOfCredits <= 89)
+                {
+                    return "Junior";
+                }
+                else 
+                {
+                    return "Senior";
+                }
+        }
 
     }
 }
